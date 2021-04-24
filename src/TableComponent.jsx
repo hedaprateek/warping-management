ffb6109cdb778ce6419c95e27b2832be877404b6
Prateek <prateek.heda@hitachiconsulting.com>
2021-04-24 16:16:01 +0530

    Updated sort and search

--- src/TableComponent.jsx
+++ src/TableComponent.jsx
@@ -1,20 +1,79 @@
- import React from "react";
- import { useSortBy, useTable } from 'react-table';
+ import { matchSorter } from "match-sorter";
+import React from "react";
+ import { useAsyncDebounce, useFilters, useGlobalFilter, useSortBy, useTable } from 'react-table';
  
+ function fuzzyTextFilterFn(rows, id, filterValue) {
+  return matchSorter(rows, filterValue, { keys: [row => row.values[id]] })
+}
+
+// Let the table remove the filter if the string is empty
+fuzzyTextFilterFn.autoRemove = val => !val
  function TableComponent(props) {
      const filter = ""; // TODO : Later for filtering
  
+  //    const defaultColumn = React.useMemo(
+  //   () => ({
+  //     // Let's set up our default Filter UI
+  //     Filter: DefaultColumnFilter,
+  //   }),
+  //   []
+  // )
+
    const {
      getTableProps,
      getTableBodyProps,
      headerGroups,
      rows,
      prepareRow,
+     state,
+    visibleColumns,
+    preGlobalFilteredRows,
+    setGlobalFilter,
    } = useTable({ columns: props.columns, data: props.data },
-    useSortBy)
+    useFilters,
+    useGlobalFilter,  useSortBy)
+
+    // Define a default UI for filtering
+function GlobalFilter({
+  preGlobalFilteredRows,
+  globalFilter,
+  setGlobalFilter,
+}) {
+  const count = preGlobalFilteredRows.length
+  const [value, setValue] = React.useState(globalFilter)
+  const onChange = useAsyncDebounce(value => {
+    setGlobalFilter(value || undefined)
+  }, 200)
+
+  return (
+    <span>
+      Search:{' '}
+      <input
+        value={value || ""}
+        onChange={e => {
+          setValue(e.target.value);
+          onChange(e.target.value);
+        }}
+        placeholder={`${count} records...`}
+        style={{
+          fontSize: '1.1rem',
+          border: '0',
+        }}
+      />
+    </span>
+  )
+}
  
    return (
+     <div>
+       <GlobalFilter
+                preGlobalFilteredRows={preGlobalFilteredRows}
+                globalFilter={state.globalFilter}
+                setGlobalFilter={setGlobalFilter}
+              />
+     
      <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
+        
        <thead>
          {headerGroups.map(headerGroup => (
            <tr {...headerGroup.getHeaderGroupProps()}>
@@ -66,6 +125,7 @@
          })}
        </tbody>
      </table>
+     </div>
    )
  }
 
