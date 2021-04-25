0b05b5d65081ff1ff6d375d61f6bec6974726b34
Prateek <prateek.heda@hitachiconsulting.com>
2021-04-25 18:04:36 +0530

    Addition of party name and quality name

--- src/Inwards.jsx
+++ src/Inwards.jsx
@@ -1,89 +1,150 @@
-import { Button, FormControlLabel, Grid, Radio, RadioGroup, TextField } from "@material-ui/core";
-import axios from "axios";
-import React, { useState } from "react";
-import DraggableDialog from "./DraggableDialog";
-import SelectComponent from "./selectComponent";
-import TableComponent from "./TableComponent";
-
+import {
+  Button,
+  FormControlLabel,
+  Grid,
+  Radio,
+  RadioGroup,
+  TextField,
+} from '@material-ui/core';
+import axios from 'axios';
+import React, { useState } from 'react';
+import DraggableDialog from './DraggableDialog';
+import SelectComponent from './selectComponent';
+import TableComponent from './TableComponent';
 
 function InwardDialog({ ...props }) {
-
   const [inwardValue, setInwardValue] = useState({});
 
   function updateInwardValues(e) {
-
     setInwardValue((prevValue) => {
-      return { ...prevValue, [e.target.id]: e.target.value }
+      return { ...prevValue, [e.target.id]: e.target.value };
     });
   }
 
   function callbackFromChild(childData) {
     setInwardValue((prevValue) => {
-      return { ...prevValue, [childData.id]: childData.value }
-    }); 
-   }
+      return { ...prevValue, [childData.id]: childData.value };
+    });
+  }
 
   return (
-    <DraggableDialog sectionTitle="Inward"  {...props} onSave={() => { props.onSave(inwardValue) }} >
-
-        <SelectComponent selectionOptions = {props.qualities.map((quality)=>({label: quality.name, value: quality.id, id:"qualityId"}))} parentCallback={callbackFromChild}/>
-        <br/>
-        <SelectComponent selectionOptions = {props.parties.map((party)=>({label: party.name, value: party.id, id: "partyId"}))} parentCallback={callbackFromChild}/>
-        <br/>
+    <DraggableDialog
+      sectionTitle="Inward"
+      {...props}
+      onSave={() => {
+        props.onSave(inwardValue);
+      }}
+    >
+      <SelectComponent
+        selectionOptions={props.qualities.map((quality) => ({
+          label: quality.name,
+          value: quality.id,
+          id: 'qualityId',
+        }))}
+        parentCallback={callbackFromChild}
+      />
+      <br />
+      <SelectComponent
+        selectionOptions={props.parties.map((party) => ({
+          label: party.name,
+          value: party.id,
+          id: 'partyId',
+        }))}
+        parentCallback={callbackFromChild}
+      />
+      <br />
       <Grid container spacing={2}>
         <Grid item lg={6} md={6} sm={12} xs={12}>
-          <TextField label="Date" variant="outlined" fullWidth id="date" value={inwardValue.date} onChange={updateInwardValues} />
+          <TextField
+            label="Date"
+            variant="outlined"
+            fullWidth
+            id="date"
+            value={inwardValue.date}
+            onChange={updateInwardValues}
+          />
         </Grid>
         <Grid item lg={6} md={6} sm={12} xs={12}>
-          <TextField label="Gatepass" variant="outlined" fullWidth id="gatepass" value={inwardValue.desc} onChange={updateInwardValues} />
+          <TextField
+            label="Gatepass"
+            variant="outlined"
+            fullWidth
+            id="gatepass"
+            value={inwardValue.desc}
+            onChange={updateInwardValues}
+          />
         </Grid>
         <Grid item lg={6} md={6} sm={12} xs={12}>
-          <TextField label="Quantity bags" variant="outlined" fullWidth id="qtyBags" value={inwardValue.name} onChange={updateInwardValues} />
+          <TextField
+            label="Quantity bags"
+            variant="outlined"
+            fullWidth
+            id="qtyBags"
+            value={inwardValue.name}
+            onChange={updateInwardValues}
+          />
         </Grid>
         <Grid item lg={6} md={6} sm={12} xs={12}>
-          <TextField label="Quantity Cones" variant="outlined" fullWidth id="qtyCones" value={inwardValue.desc} onChange={updateInwardValues} />
+          <TextField
+            label="Quantity Cones"
+            variant="outlined"
+            fullWidth
+            id="qtyCones"
+            value={inwardValue.desc}
+            onChange={updateInwardValues}
+          />
         </Grid>
         <Grid item lg={6} md={6} sm={12} xs={12}>
-          <TextField label="Lot Number" variant="outlined" fullWidth id="lotNo" value={inwardValue.name} onChange={updateInwardValues} />
+          <TextField
+            label="Lot Number"
+            variant="outlined"
+            fullWidth
+            id="lotNo"
+            value={inwardValue.name}
+            onChange={updateInwardValues}
+          />
         </Grid>
         <Grid item lg={6} md={6} sm={12} xs={12}>
-          <TextField label="Net Weight" variant="outlined" fullWidth id="netWt" value={inwardValue.desc} onChange={updateInwardValues} />
+          <TextField
+            label="Net Weight"
+            variant="outlined"
+            fullWidth
+            id="netWt"
+            value={inwardValue.desc}
+            onChange={updateInwardValues}
+          />
         </Grid>
       </Grid>
     </DraggableDialog>
   );
 }
 class Inwards extends React.Component {
-
   componentDidMount() {
-    axios.get(`/api/parties`)
-      .then(res => {
-        const parties = res.data;
-        this.setState({ parties });
-        console.log("parties", parties)
-      })
-      axios.get(`/api/qualities`)
-      .then(res => {
-        const qualities = res.data;
-        this.setState({ qualities });
-        console.log("qualities", qualities)
-      })
-      axios.get(`/api/inward`)
-      .then(res => {
-        const inward = res.data;
-        this.setState({ inward });
-        console.log("Inward", inward)
-      })
+    axios.get(`/api/parties`).then((res) => {
+      const parties = res.data;
+      this.setState({ parties });
+      console.log('parties', parties);
+    });
+    axios.get(`/api/qualities`).then((res) => {
+      const qualities = res.data;
+      this.setState({ qualities });
+      console.log('qualities', qualities);
+    });
+    axios.get(`/api/inward`).then((res) => {
+      const inward = res.data;
+      this.setState({ inward });
+      console.log('Inward', inward);
+    });
   }
 
   state = {
     parties: [],
     qualities: [],
     inward: [],
-    filter: "",
+    filter: '',
     dialogOpen: false,
 
-    columns : [
+    columns: [
       {
         Header: '',
         accessor: 'functionButtons', // accessor is the "key" in the data
@@ -94,7 +155,18 @@ class Inwards extends React.Component {
       },
       {
         Header: 'PARTY NAME',
-        accessor: 'partyName',
+        accessor: 'partyId',
+        Cell: ({ value }) => {
+          let partyName = [];
+          if (value) {
+            partyName = this.state.parties.filter((party) => {
+              if (party.id === value) {
+                return party;
+              }
+            });
+          }
+          return partyName[0] ? partyName[0].name : '-';
+        },
       },
       {
         Header: 'GATEPASS',
@@ -102,7 +174,18 @@ class Inwards extends React.Component {
       },
       {
         Header: 'QUALITY NAME',
-        accessor: 'qualityName',
+        accessor: 'qualityId',
+        Cell: ({ value }) => {
+          let qualityName = [];
+          if (value) {
+            qualityName = this.state.qualities.filter((quality) => {
+              if (quality.id === value) {
+                return quality;
+              }
+            });
+          }
+          return qualityName[0] ? qualityName[0].name : '-';
+        },
       },
       {
         Header: 'QUANTITY BAGS',
@@ -121,55 +204,67 @@ class Inwards extends React.Component {
         accessor: 'netWt', // accessor is the "key" in the data
       },
     ],
-  }
+  };
 
   showDialog(show) {
-    this.setState({dialogOpen: show});
+    this.setState({ dialogOpen: show });
   }
 
   saveDetails(inwardValue) {
     console.log(inwardValue);
 
-    if(!inwardValue.partyId)
-    {
+    if (!inwardValue.partyId) {
       inwardValue.partyId = '1';
-  }
-    
-    if(!inwardValue.qualityId)
-    {
+    }
+
+    if (!inwardValue.qualityId) {
       inwardValue.qualityId = '1';
     }
-    
-    axios.post(`/api/inward`, inwardValue, {
-		 headers: {
-			  'content-type': 'application/json',
-		 },
-	})
-      .then(res => {
+
+    axios
+      .post(`/api/inward`, inwardValue, {
+        headers: {
+          'content-type': 'application/json',
+        },
+      })
+      .then((res) => {
         const parties = this.state.parties;
         const latestData = res.data;
-         this.setState((prevState)=> {
-           return {parties: [
-             ...prevState.parties, latestData
-           ]}
-         })
-      })
-
+        this.setState((prevState) => {
+          return { parties: [...prevState.parties, latestData] };
+        });
+      });
 
     this.showDialog(false);
   }
 
-  render() {  
-    return <div>
-      <h2>Inwards</h2>
+  render() {
+    return (
+      <div>
+        <h2>Inwards</h2>
 
-      <Button variant="outlined" color="primary" onClick={() => this.showDialog(true)}>
-        Add Inward
-      </Button>
-      <TableComponent columns={this.state.columns} data={this.state.qualities} filterText={this.state.filter} />
-      <InwardDialog open={this.state.dialogOpen} onClose={() => this.showDialog(false)} onSave={(inwardValue) => this.saveDetails(inwardValue)} parties={this.state.parties} qualities={this.state.qualities}/>
-      </div>;
-}
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={() => this.showDialog(true)}
+        >
+          Add Inward
+        </Button>
+        <TableComponent
+          columns={this.state.columns}
+          data={this.state.inward}
+          filterText={this.state.filter}
+        />
+        <InwardDialog
+          open={this.state.dialogOpen}
+          onClose={() => this.showDialog(false)}
+          onSave={(inwardValue) => this.saveDetails(inwardValue)}
+          parties={this.state.parties}
+          qualities={this.state.qualities}
+        />
+      </div>
+    );
+  }
 }
 
 export default Inwards;
